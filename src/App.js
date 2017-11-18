85e9dd070bba643d8123df61dd34d411edb190d7
perspicacious-one <nelsondcraig@gmail.com>
2017-11-18 12:38:42 -0800

    checkpoint-6 completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-
 import './App.css';
+import ToDo from './components/ToDo';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <ul>
+          <ToDo />
+          <ToDo />
+        </ul>
       </div>
     );
   }
